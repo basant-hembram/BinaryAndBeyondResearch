9cbe320ab880207ac11d292ff6347d8fc8e21710
saurabhsindhu <sindhu.saurabh7@gmail.com>
2026-01-13 10:37:30 +0530

    test routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import ScrollToTop from './components/common/ScrollToTop';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
-import Calculator from './pages/Calculator';
-import MFResearch from './pages/MFResearch';
-import BecomeDistributor from './pages/BecomeDistributor';
-import ContactUs from './pages/ContactUs';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Disclaimer from './pages/Disclaimer';
-import TermsOfUse from './pages/TermsOfUse';
-import ImportantLinks from './pages/ImportantLinks';
-import Testimonials from './pages/Testimonials';
-import Team from './pages/Team';
-import LatestBlogs from './pages/LatestBlogs';
-import BlogDetailPage from './pages/BlogDetailPage';
+
 
 function App() {
-  return (
-    <Router>
-      <ScrollToTop />
-      <div className="min-h-screen">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/mf-research" element={<MFResearch />} />
-          <Route path="/become-distributor" element={<BecomeDistributor />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/disclaimer" element={<Disclaimer />} />
-          <Route path="/terms-of-use" element={<TermsOfUse />} />
-          <Route path="/important-links" element={<ImportantLinks />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/our-team" element={<Team />} />
-          <Route path="/latest-blogs" element={<LatestBlogs />} />
-          <Route path="/blog/:slug" element={<BlogDetailPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
